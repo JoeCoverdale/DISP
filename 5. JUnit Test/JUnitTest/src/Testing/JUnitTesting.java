8562c18342b4059a4d318ad96ff86b948e709388
JoeCoverdale <60738851+JoeCoverdale@users.noreply.github.com>
2021-05-07 19:17:21 +0100

    Add files via upload

---
+++ 5. JUnit Test/JUnitTest/src/Testing/JUnitTesting.java
@@ -0,0 +1,21 @@
+package Testing;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class JUnitTesting {
+	
+	//Class for ingredient stock 
+	public class Ingredient {
+		private String itemName;
+		private String itemNum;
+		private int itemStock;
+		
+	//Ingredient constructor
+	public Ingredient(String name, String num, int stock) {
+		itemName = name;
+		itemNum = num;
+		itemStock = stock;	
+	}
+	
+	}
+}
